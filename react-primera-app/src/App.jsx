6a10f3767a96f15321ddc57fb2b0b1190365e49a
kikemaya <enrique.mg.cd@gmail.com>
2023-09-14 20:11:58 -0600

    configuracion eslint-prettier casi lista

--- react-primera-app/src/App.jsx
+++ react-primera-app/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { React, useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './vite.svg'
 import './App.css'
 
 function App() {
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -25,9 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Mi texto personalizadooooo</p>
     </>
   )
 }
